054ef63fa9d763a03233685e566cfbaa193bc4a2
xuzhu <xzhu1900@gmail.com>
2018-11-26 15:15:14 -0800

    update with file loading logic

--- torch/csrc/api/src/data/htk/htk_parser.cpp
+++ torch/csrc/api/src/data/htk/htk_parser.cpp
@@ -1,21 +1,13 @@
-#include "htk_parser.hpp"
+#include "htk_parser.h"
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
 using boost::property_tree::ptree;
 using boost::property_tree::read_json;
 
-union {
-    uint16_t s;
-    unsigned char c[2];
-} constexpr static endian_checker{1};
 
-constexpr bool is_little_endian()
-{
-    return endian_checker.c[0] == 1;
-}
 
-bool HTKParser::is_little_endian_ = is_little_endian();
+//bool HTKParser::is_little_endian_ = is_little_endian();
 
 HTKParser::HTKParser(const string &file_directory, const string &file_set_name) : file_directory_(file_directory)
 {
@@ -54,32 +46,28 @@ std::vector<Utterance> HTKParser::parse_chunk(size_t chunk_id)
         throw std::runtime_error(error_msg);
     }
 
-    assert(!m_chunk_file_stream.is_open());
-
     for (auto &extension : file_extensions_)
     {
-        for (auto &chunk_info : chunk_info_list_)
-        {
-            std::string file_full_name = file_directory_ + chunk_info.file_name + "." + extension;
-            result.resize(chunk_info.chunk_size);
+        std::string file_full_name = file_directory_ + "chunk" + std::to_string(chunk_id) + "." + extension;
+        size_t example_count_in_chunk = chunk_info_list_[chunk_id].chunk_size;
+        result.resize(example_count_in_chunk);
 
-            if (extension == "feature")
-            {
-                parse_data<FeatureType>(file_full_name, "Feature", chunk_info.chunk_size, result);
-            }
-            else if (extension == "lattice")
-            {
-                parse_data<LatticeType>(file_full_name, "Lattice", chunk_info.chunk_size, result);
-            }
-            else if (extension == "label")
-            {
-                parse_data<LabelType>(file_full_name, "Label", chunk_info.chunk_size, result);
-            }
-            else
-            {
-                std::string error_msg = "unknown field " + extension;
-                throw std::runtime_error(error_msg);
-            }
+        if (extension == "feature")
+        {
+            parse_data<FeatureType>(file_full_name, "Feature", example_count_in_chunk, result);
+        }
+        else if (extension == "lattice")
+        {
+            parse_data<LatticeType>(file_full_name, "Lattice", example_count_in_chunk, result);
+        }
+        else if (extension == "label")
+        {
+            parse_data<LabelType>(file_full_name, "Label", example_count_in_chunk, result);
+        }
+        else
+        {
+            std::string error_msg = "unknown field " + extension;
+            throw std::runtime_error(error_msg);
         }
     }
 
@@ -105,13 +93,3 @@ void ValidateTagMatch(const char *data, const char *header, size_t dataLen, size
     }
 }
 
-void HTKParser::ReadString(ifstream& reader, string_view tagName)
-{
-#ifdef _DEBUG
-    std::vector<char> header(tagName.size());
-    reader.read(header.data(), header.size());
-    ValidateTagMatch(header.data(), tagName.data(), header.size(), tagName.size());
-#else
-    reader.ignore(tagName.size() - 1);
-#endif
-}
